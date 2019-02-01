6f2c26dd18207b7d66a1048213821f315f123bff
aaronglick07 <aaronglick07@gmail.com>
2019-02-01 12:45:08 -0800

    Unit01 Fixed

---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,41 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes flag = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      flag.printTwoBlankLines();
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printASmallBox();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      
+   }
+}
