c9aece3b547707664045f8b312f200a90ef6adab
AaronTheNerd <aaronthebird07@gmail.com>
2019-02-26 20:49:12 -0800

    Unit 8 Complete

--- Unit08/src/RayOddToEven.java
+++ Unit08/src/RayOddToEven.java
@@ -7,18 +7,20 @@ public class RayOddToEven
 {
 	public static int go(int[] ray)
 	{
-		int count = -1;
-		int i = -1;
-		
-		do{
-			if(ray[Math.abs(i)] % 2 == 0) {
-				
+		int oddCount = -1;
+		int evenCount = -1;
+		boolean isOdd = false;
+		for (int i = 0; i < ray.length-1; i++) {
+			if(ray[i] % 2 == 1 && oddCount < 0){
+				oddCount = i;
+				isOdd = true;
 			}
-			i++;
-			count++;
-			
-		}while(ray[i] % 2 ==1);
-		if(count == 0) return -1;
-		return count;
+			if(ray[i] % 2 ==0 && evenCount <0 && isOdd){
+				evenCount = i;
+			}
+		}
+		if(oddCount == -1 || evenCount == -1) return -1;
+		return Math.abs(evenCount - oddCount);
+		
 	}
 }
